2c2b1ad62f179970a2d91beba55f798b19aca845
HerPerception <offorbuzor1@gmail.com>
2026-08-27 15:47:19 +0000

    Updates to the Journal Project - Day 2 (AI Integration)

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import json
 import requests
 import os
 
@@ -13,15 +14,27 @@ def banana():
 def save_entry():
     user_entry = request.form["entry_text"]
     api_key = os.environ.get("GOOGLE_API_KEY")
-    model = "gemini-2.5-flash"
+    model = "gemini-3.7-flash"
     url = f"https://generativelanguage.googleapis.com/v1beta/models/{model}:generateContent?key={api_key}"
-
+    prompt = f"Read this entry {user_entry}, predict the mood in one word, give a mood score on the scale of 1-10, and a two-sentence reflection. Return in JSON format, for example {{'mood_label': 'stressed', 'mood_score': 4, 'reflection': '...'}}"
     response = requests.post(
         url,
         headers={"Content-Type": "application/json"},
-        json={...}
+        json={
+                "contents": [
+                    {"parts": [{"text": prompt}]}
+                ]
+            }
     )
-    return user_entry
+    data = response.json()
+    print(data)
+    first_candidate = data["candidates"][0]
+    model_text = first_candidate["content"]["parts"][0]["text"]
+    model_text = model_text.replace("```json", "")
+    model_text = model_text.replace("```", "")
+    formatted_text = json.loads(model_text)
+
+    return formatted_text["mood_label"]
 
 if __name__ == "__main__":
     app.run(debug=True)
