8636a2b9206749576334aa3760afb221e0361f92
HerPerception <offorbuzor1@gmail.com>
2026-09-01 15:14:20 +0000

    First Major Milestone - LLM Output is printed correctly

--- app.py
+++ app.py
@@ -13,28 +13,37 @@ def banana():
 @app.route("/entries", methods=["POST"])
 def save_entry():
     user_entry = request.form["entry_text"]
-    api_key = os.environ.get("GOOGLE_API_KEY")
-    model = "gemini-3.7-flash"
-    url = f"https://generativelanguage.googleapis.com/v1beta/models/{model}:generateContent?key={api_key}"
+    api_key = os.environ.get("GROQ_API_KEY")
+    model = "qwen/qwen3.6-27b"
+    url = "https://api.groq.com/openai/v1/chat/completions"
     prompt = f"Read this entry {user_entry}, predict the mood in one word, give a mood score on the scale of 1-10, and a two-sentence reflection. Return in JSON format, for example {{'mood_label': 'stressed', 'mood_score': 4, 'reflection': '...'}}"
     response = requests.post(
         url,
-        headers={"Content-Type": "application/json"},
+        headers={"Content-Type": "application/json", "Authorization": f"Bearer {api_key}"},
         json={
-                "contents": [
-                    {"parts": [{"text": prompt}]}
+                "model": model,
+                "messages": [
+                    {"role": "user", "content": prompt}
                 ]
-            }
+        }
     )
     data = response.json()
     print(data)
-    first_candidate = data["candidates"][0]
-    model_text = first_candidate["content"]["parts"][0]["text"]
+    first_choice = data["choices"][0]
+    model_text = first_choice["message"]["content"]
+    index = model_text.find("</think>")
+    model_text = model_text[index+len("</think>"):]
     model_text = model_text.replace("```json", "")
     model_text = model_text.replace("```", "")
     formatted_text = json.loads(model_text)
 
-    return formatted_text["mood_label"]
+    mood_label = formatted_text["mood_label"]
+    mood_score = formatted_text["mood_score"]
+    reflection = formatted_text["reflection"]
+
+    print(f"Based on the journal entry, the mood is predicted to be: {mood_label}, with mood score: {mood_score}, and reflection: {reflection}"
+)
+    return f"Based on the journal entry, the mood is predicted to be: {mood_label}, with mood score: {mood_score}, and reflection: {reflection}"
 
 if __name__ == "__main__":
     app.run(debug=True)
